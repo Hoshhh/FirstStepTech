44b361ca7ea08b90f3d0842978c97602e61fe7e1
Hoshhh <nyxious123@live.com>
2023-07-24 21:45:37 -0400

    Auth file setup

---
+++ src/lib/auth.ts
@@ -0,0 +1,24 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { db } from "@/lib/db";
+
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(db),
+    session: { 
+        strategy: "jwt" 
+    },
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        })
+    ]
+
+}
