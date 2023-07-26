c5bb3c38fe1c742774f272b2cb66fad38f6d79f1
Hoshhh <nyxious123@live.com>
2023-07-25 23:48:28 -0400

    sess testing

--- src/app/(site)/(Landing Page)/page.tsx
+++ src/app/(site)/(Landing Page)/page.tsx
@@ -1,8 +1,11 @@
+import { getCurrentUser } from '@/lib/session'
 import Hero from './(components)/Hero'
 import Info from './(components)/Info'
 import Navbar from './(components)/Navbar'
 
-export default function Home() {
+export default async function Home() {
+  const user = await getCurrentUser()
+  console.log(user)
   return (
     <div>
       <Navbar />
